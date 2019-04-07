0b7b57c823fb0784a506de5a0ba427a0e4616ac1
kekland <kk.erzhan@gmail.com>
2019-04-07 16:22:41 +0600

    impl: PID tuning results now save in SD card.

--- src/tuner/pid_tuner.cpp
+++ src/tuner/pid_tuner.cpp
@@ -22,12 +22,17 @@ void tuneAutoaim()
   info("Starting to tune autoaim.", "tuneAutoaim");
   auto input = VisionAimInputController(Flag::red);
   auto output = DrivetrainTurnController();
-  
+
   auto tuner = PIDTunerFactory::create(std::make_shared<okapi::ControllerInput<double>>(input),
                                        std::make_shared<okapi::ControllerOutput<double>>(output), 
                                        2_s, 0, 0.0, 0.2, 0.0, 0.2, 0.0, 0.2, 20);
   
   auto values = tuner.autotune();
   printf("Tuned: %f %f %f\n", values.kP, values.kI, values.kD);
+
+  FILE* outputFile = fopen("pid_output.txt", "w");
+  fprintf(outputFile, "%f %f %f\n", values.kP, values.kI, values.kD);
+  fclose(outputFile);
+  
   info("Finished tuning.", "tuneAutoaim");
 }
