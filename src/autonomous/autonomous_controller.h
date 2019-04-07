7cc15b011ca7fbb713a703a92bce714f815ebd06
kekland <kk.erzhan@gmail.com>
2019-04-07 14:51:50 +0600

    fix: forgot to update .h

--- src/autonomous/autonomous_controller.h
+++ src/autonomous/autonomous_controller.h
@@ -1,10 +1,11 @@
 #ifndef AUTONOMOUS_CONTROLLER_H_
 #define AUTONOMOUS_CONTROLLER_H_
 #include "main.h"
+using namespace okapi;
 
 void stop();
 
-void drive(double cm);
-void turn(double targetDegrees);
+void drive(QLength cm);
+void turn(QAngle targetDegrees);
 
 #endif
--- src/autonomous_variants/autonomous_front_1.cpp
+++ src/autonomous_variants/autonomous_front_1.cpp
@@ -2,10 +2,12 @@
 #include "autonomous_variants.h"
 #include "../autonomous/autonomous_controller.h"
 
+using namespace okapi;
+
 void autonomousFront1() {
   Alliance alliance = gameState.getAlliance();
   Flag opposingFlag = gameState.getOpposingFlag();
 
-  drive(100.0);
-  
+  drive(100.0_cm);
+  turn(90.0_deg);
 }
